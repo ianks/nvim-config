243cc7c8ee8d8e4fd24d079eada542bdbe2d3446
Ian Ker-Seymer <ian.kerseymer@shopify.com>
2025-08-03 17:23:08 -0400

    Update

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -1,116 +1,35 @@
--- AstroLSP allows you to customize the features in AstroNvim's LSP configuration engine
--- Configuration documentation can be found with `:h astrolsp`
--- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
---       as this provides autocomplete and documentation while editing
-
 ---@type LazySpec
 return {
   "AstroNvim/astrolsp",
   ---@type AstroLSPOpts
   opts = {
-    -- Configuration table of features provided by AstroLSP
     features = {
-      codelens = true, -- enable/disable codelens refresh on start
-      inlay_hints = false, -- enable/disable inlay hints on start
-      semantic_tokens = true, -- enable/disable semantic token highlighting
+      codelens = true,
+      inlay_hints = false,
+      -- semantic_tokens = true,
     },
-    -- customize lsp formatting options
     formatting = {
       -- control auto formatting on save
       format_on_save = {
         enabled = true,
-        ignore_filetypes = { "c", "cpp" }, -- Your preference
-      },
-      disabled = { -- disable formatting capabilities for the listed language servers
-        -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
-        -- "lua_ls",
+        ignore_filetypes = { "c", "cpp", "yaml" }, -- Your preference
       },
       timeout_ms = 1000, -- default format timeout
-      -- filter = function(client) -- fully override the default formatting function
-      --   return true
-      -- end
     },
-    -- enable servers that you already have installed without mason
     servers = {
-      -- "pyright"
       "ruby_lsp",
-      "rubocop",
     },
-    -- customize language server configuration options passed to `lspconfig`
     ---@diagnostic disable: missing-fields
     config = {
-      -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
-      ruby_lsp = {
-        cmd = vim.fn.executable "shadowenv" == 1 and {
-          "shadowenv",
-          "exec",
-          "--",
-          "env",
-          "RUBYOPT=-EUTF-8:UTF-8",
-          "ruby-lsp",
-        } or { "env", "RUBYOPT=-EUTF-8:UTF-8", "ruby-lsp" },
-        filetypes = { "ruby", "eruby" },
-        -- root_dir = require("lspconfig.util").root_pattern("Gemfile", ".git"),
-        init_options = {
-          -- formatter = "rubocop",
-          -- linters = { "rubocop" },
-          -- theres a bug with rubocop: https://github.com/Shopify/ruby-lsp/pull/3612
-          formatter = "none",
-          linters = {},
-
-          enabledFeatures = {
-            diagnostics = true,
-            formatting = false,
-          },
-        },
-        capabilities = {
-          general = {
-            positionEncodings = { "utf-16" },
-          },
-        },
-      },
-      rubocop = {
-        cmd = vim.fn.executable "shadowenv" == 1
-            and { "shadowenv", "exec", "--", "bundle", "exec", "rubocop", "--lsp" }
-          or { "bundle", "exec", "rubocop", "--lsp" },
-        filetypes = { "ruby", "eruby" },
-        -- root_dir = require("lspconfig.util").root_pattern("Gemfile", ".git"),
-        settings = {
-          useBundler = true,
-          enableExperimentalFeatures = true,
-          autoCorrect = true, -- set to true if you want to enable autocorrect on save
-        },
-        capabilities = {
-          general = {
-            positionEncodings = { "utf-16" },
-          },
-        },
-      },
+      clangd = { capabilities = { offsetEncoding = "utf-8" } },
     },
-    -- customize how language servers are attached
-    handlers = {
-      -- a function without a key is simply the default handler, functions take two parameters, the server name and the configured options table for that server
-      -- function(server, opts) require("lspconfig")[server].setup(opts) end
-
-      -- the key is the server that is being setup with `lspconfig`
-      -- rust_analyzer = false, -- setting a handler to false will disable the set up of that language server
-      -- pyright = function(_, opts) require("lspconfig").pyright.setup(opts) end -- or a custom handler function can be passed
-    },
+    handlers = {},
     -- Configure buffer local auto commands to add when attaching a language server
     autocmds = {
-      -- first key is the `augroup` to add the auto commands to (:h augroup)
       lsp_codelens_refresh = {
-        -- Optional condition to create/delete auto command group
-        -- can either be a string of a client capability or a function of `fun(client, bufnr): boolean`
-        -- condition will be resolved for each client on each execution and if it ever fails for all clients,
-        -- the auto commands will be deleted for that buffer
         cond = "textDocument/codeLens",
-        -- cond = function(client, bufnr) return client.name == "lua_ls" end,
-        -- list of auto commands to set
         {
-          -- events to trigger
           event = { "InsertLeave", "BufEnter" },
-          -- the rest of the autocmd options (:h nvim_create_autocmd)
           desc = "Refresh codelens (buffer)",
           callback = function(args)
             if require("astrolsp").config.features.codelens then vim.lsp.codelens.refresh { bufnr = args.buf } end
@@ -118,7 +37,6 @@ return {
         },
       },
     },
-    -- mappings to be set up on attaching of a language server
     mappings = {
       n = {
         -- a `cond` key can provided as the string of a server capability to be required to attach, or a function with `client` and `bufnr` parameters from the `on_attach` that returns a boolean
@@ -127,13 +45,12 @@ return {
           desc = "Declaration of current symbol",
           cond = "textDocument/declaration",
         },
+        ["<C-.>"] = {
+          function() vim.lsp.buf.code_action() end,
+          desc = "Code action",
+          cond = "textDocument/codeAction",
+        },
       },
     },
-    -- A custom `on_attach` function to be run after the default `on_attach` function
-    -- takes two parameters `client` and `bufnr`  (`:h lspconfig-setup`)
-    on_attach = function(client, bufnr)
-      -- this would disable semanticTokensProvider for all clients
-      -- client.server_capabilities.semanticTokensProvider = nil
-    end,
   },
 }
--- lua/plugins/ruby.lua
+++ lua/plugins/ruby.lua
@@ -1,12 +1,3 @@
-local function maybe_shadowenv_exec(...)
-  local args = { ... } -- capture all arguments
-  local root = require("lspconfig.util").root_pattern("dev.yml", "Gemfile.lock", "Gemfile")
-  if vim.fn.executable "shadowenv" == 1 then
-    return vim.list_extend({ "shadowenv", "exec", "--dir", root, "--" }, args)
-  end
-  return args
-end
-
 return {
   {
     "nvim-treesitter/nvim-treesitter",
@@ -21,38 +12,43 @@ return {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       -- Remove ruby-lsp from ensure_installed to prevent Mason from auto-installing it
-      -- opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "ruby-lsp" })
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "ruby-lsp" })
     end,
   },
-  -- Ruby LSP configuration is now in astrolsp.lua
-  -- {
-  --   "AstroNvim/astrolsp",
-  --   optional = true,
-  --   opts = function(_, opts)
-  --     -- Add ruby_lsp to the servers list to ensure it's set up
-  --     opts.servers = opts.servers or {}
-  --     vim.list_extend(opts.servers, { "ruby_lsp" })
-  --
-  --     -- Configure ruby_lsp
-  --     opts.config = opts.config or {}
-  --     opts.config.ruby_lsp = {
-  --       cmd = maybe_shadowenv_exec "ruby-lsp",
-  --       -- cmd = { "ruby-lsp" },
-  --       filetypes = { "ruby", "eruby" },
-  --       root_dir = require("lspconfig.util").root_pattern("dev.yml", "Gemfile.lock", "Gemfile"),
-  --       init_options = {
-  --         formatter = "auto",
-  --       },
-  --       -- root directory detection for detecting the project root
-  --       -- root_dir = require("lspconfig.util").root_pattern("dev.yml", "Gemfile.lock", "Gemfile"),
-  --     }
-  --
-  --     return opts
-  --   end,
-  -- },
   {
-    "mfussenegger/nvim-dap",
+    "AstroNvim/astrolsp",
     optional = true,
-    dependencies = { "suketa/nvim-dap-ruby", config = true },
+    opts = function(_, opts)
+      -- Add ruby_lsp to the servers list to ensure it's set up
+      opts.servers = opts.servers or {}
+      vim.list_extend(opts.servers, { "ruby_lsp" })
+
+      -- Configure ruby_lsp
+      opts.config = opts.config or {}
+      opts.config.ruby_lsp = {
+        cmd = vim.fn.executable "shadowenv" == 1
+            and {
+              "shadowenv",
+              "exec",
+              "--",
+              -- "env",
+              -- "RUBYOPT=-EUTF-8:UTF-8",
+              "ruby-lsp",
+            }
+          or { "ruby-lsp" },
+        filetypes = { "ruby", "eruby" },
+        root_dir = require("lspconfig.util").root_pattern("dev.yml", "Gemfile.lock", "Gemfile"),
+        init_options = {
+          formatter = "auto",
+        },
+        capabilities = {
+          general = {
+            positionEncodings = { "utf-16" },
+          },
+        },
+      }
+
+      return opts
+    end,
   },
 }
